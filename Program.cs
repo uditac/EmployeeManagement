5fc89373915b89c80d7e4230514aa897dc587f99
udita.chakraborty <mail2udita@yahoo.com>
2023-04-28 00:19:58 +0200

    added serilog and Iconfiguration

--- Program.cs
+++ Program.cs
@@ -1,24 +1,37 @@
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-//logging
 
 using var log = new LoggerConfiguration()//new
     .WriteTo.File("./logs.txt")
     .CreateLogger();
 
+
+
+var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<Serilog.ILogger>(log);
 log.Information("started logging");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+ConfigurationManager configuration = builder.Configuration; // allows both to access and to set up the config
+IWebHostEnvironment environment = builder.Environment;
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+var app = builder.Build();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 
 // Configure the HTTP request pipeline.
